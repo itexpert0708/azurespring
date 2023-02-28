74bce5dfe4133f2bebc07b94a059389f8082ce4d
jsna <itexpert0708@gmail.com>
2023-02-28 16:32:09 +0900

    first

---
+++ src/main/java/com/example/azurespring/AzurespringApplication.java
@@ -0,0 +1,13 @@
+package com.example.azurespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurespringApplication.class, args);
+	}
+
+}
